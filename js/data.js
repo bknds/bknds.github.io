899c07007b90d0eaf987f9d59f88acf1bec29c1d
sevenzzw <Librazzw@163.com>
2017-11-27 17:13:25 +0800

    11

---
+++ js/data.js
@@ -0,0 +1,24 @@
+var SHIP_DATA = [
+    { name: '电话卡机啥都看电话卡机啥都看电话卡机啥都看电话卡机啥都看', price: '100',oldprice:'250', img: '../sevenzzw.github.io/img/11.jpg' },
+    { name: '电话卡机啥都看电话卡机啥都看电话卡机啥都看电话卡机啥都看', price: '100',oldprice:'250', img: '../sevenzzw.github.io/img/11.jpg' },
+    { name: '电话卡机啥都看电话卡机啥都看电话卡机啥都看电话卡机啥都看', price: '100',oldprice:'250', img: '../sevenzzw.github.io/img/11.jpg' },
+    { name: '电话卡机啥都看电话卡机啥都看电话卡机啥都看电话卡机啥都看', price: '100',oldprice:'250', img: '../sevenzzw.github.io/img/11.jpg' },
+    { name: '电话卡机啥都看电话卡机啥都看电话卡机啥都看电话卡机啥都看', price: '100',oldprice:'250', img: '../sevenzzw.github.io/img/11.jpg' },
+    { name: '电话卡机啥都看电话卡机啥都看电话卡机啥都看电话卡机啥都看', price: '100',oldprice:'250', img: '../sevenzzw.github.io/img/11.jpg' },
+    { name: '电话卡机啥都看电话卡机啥都看电话卡机啥都看电话卡机啥都看', price: '100',oldprice:'250', img: '../sevenzzw.github.io/img/11.jpg' },
+    { name: '电话卡机啥都看电话卡机啥都看电话卡机啥都看电话卡机啥都看', price: '100',oldprice:'250', img: '../sevenzzw.github.io/img/11.jpg' },
+    { name: '电话卡机啥都看电话卡机啥都看电话卡机啥都看电话卡机啥都看', price: '100',oldprice:'250', img: '../sevenzzw.github.io/img/11.jpg' },
+    { name: '电话卡机啥都看电话卡机啥都看电话卡机啥都看电话卡机啥都看', price: '100',oldprice:'250', img: '../sevenzzw.github.io/img/11.jpg' },
+    { name: '电话卡机啥都看电话卡机啥都看电话卡机啥都看电话卡机啥都看', price: '100',oldprice:'250', img: '../sevenzzw.github.io/img/11.jpg' },
+    { name: '电话卡机啥都看电话卡机啥都看电话卡机啥都看电话卡机啥都看', price: '100',oldprice:'250', img: '../sevenzzw.github.io/img/11.jpg' },
+    { name: '电话卡机啥都看电话卡机啥都看电话卡机啥都看电话卡机啥都看', price: '100',oldprice:'250', img: '../sevenzzw.github.io/img/11.jpg' },
+    { name: '电话卡机啥都看电话卡机啥都看电话卡机啥都看电话卡机啥都看', price: '100',oldprice:'250', img: '../sevenzzw.github.io/img/11.jpg' },
+    { name: '电话卡机啥都看电话卡机啥都看电话卡机啥都看电话卡机啥都看', price: '100',oldprice:'250', img: '../sevenzzw.github.io/img/11.jpg' },
+];
+
+var shipInfo = new Vue({
+    el:'.ship-wrapper',
+    data: {
+        shipData: SHIP_DATA
+    }
+})
